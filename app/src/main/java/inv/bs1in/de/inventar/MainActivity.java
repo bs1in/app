e11781b9bfa006fca314c2c70fe63a644d33c04c
JakobHeubl <Jakob.Heubl@sahnee.de>
2019-07-04 14:56:08 +0200

    Hinzufügen von Nachrichten an den User

--- app/src/main/java/inv/bs1in/de/inventar/MainActivity.java
+++ app/src/main/java/inv/bs1in/de/inventar/MainActivity.java
@@ -1,9 +1,11 @@
 package inv.bs1in.de.inventar;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnReport;
     private TextView txtId;
     private TextView txtDesc;
-    //private CheckBox chkConfirm;
+    private CheckBox chkConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +22,26 @@ public class MainActivity extends AppCompatActivity {
 
         this.txtId = findViewById(R.id.txtId);
         this.txtDesc = findViewById(R.id.txtDesc);
-        //this.chkConfirm = findViewById(R.id.chkConfirm);
+        this.chkConfirm = findViewById(R.id.chkConfirm);
         this.btnReport = findViewById(R.id.btnConfirm);
         this.btnReport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                boolean confirm = true;//MainActivity.this.chkConfirm.isChecked();
+                boolean confirm = MainActivity.this.chkConfirm.isChecked();
+
                 if (confirm) {
                     Ticket t = MainActivity.this.createTicket();
+                    sendMessage("Dein Ticket wurde angelegt!", "Erfolgreich!");
+
                     try {
                         Ticket.createTicket(t);
                     } catch (Exception e1) {
                         e1.printStackTrace();
                     }
+
+                }
+                else {
+                    sendMessage("Du musst die Checkbox bestätigen!", "Fehler!");
                 }
             }
         });
@@ -44,5 +53,15 @@ public class MainActivity extends AppCompatActivity {
         t.description = this.txtDesc.getText().toString();
         t.done = false;
         return t;
+    }
+
+    private void sendMessage(String message, String title) {
+        AlertDialog.Builder msg = new AlertDialog.Builder(this);
+        msg.setTitle(title);
+        msg.setMessage(message);
+        msg.setCancelable(true);
+
+        AlertDialog msgDialog = msg.create();
+        msgDialog.show();
     }
 }
